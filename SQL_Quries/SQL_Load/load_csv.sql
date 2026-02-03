9a56954cb422f185cb6d68bcc043a6f80c688bba
Ahamed Nismi <mnanismi2000@gmail.com>
2026-02-03 08:40:25 +0530

    date Table Created

--- SQL_Quries/SQL_Load/load_csv.sql
+++ SQL_Quries/SQL_Load/load_csv.sql
@@ -7,5 +7,8 @@ COPY customer
 FROM 'D:\Mine\SQL\sql_analysis_colab\csv_files\customer.csv'
 WITH  (FORMAT csv ,HEADER TRUE , DELIMITER ',' , ENCODING 'UTF8');
 
+SELECT COUNT(*) FROM customer;
 
-
+COPY date
+FROM 'D:\Mine\SQL\sql_analysis_colab\csv_files\date.csv'
+WITH (FORMAT csv , HEADER TRUE , DELIMITER ',',ENCODING'UTF8');
