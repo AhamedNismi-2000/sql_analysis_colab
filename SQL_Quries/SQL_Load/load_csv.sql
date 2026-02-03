f89c6a6be349016e7164d06320c18564d9f5d7f8
Ahamed Nismi <mnanismi2000@gmail.com>
2026-02-03 08:35:10 +0530

    Cutomer Table Created

--- SQL_Quries/SQL_Load/load_csv.sql
+++ SQL_Quries/SQL_Load/load_csv.sql
@@ -3,6 +3,9 @@ COPY  currencyexchange
 FROM  'D:\Mine\SQL\sql_analysis_colab\csv_files\currencyexchange.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
+COPY customer
+FROM 'D:\Mine\SQL\sql_analysis_colab\csv_files\customer.csv'
+WITH  (FORMAT csv ,HEADER TRUE , DELIMITER ',' , ENCODING 'UTF8');
 
 
 
