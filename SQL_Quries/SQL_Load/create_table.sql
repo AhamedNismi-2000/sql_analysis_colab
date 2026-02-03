27628cd1be0cc4b6cc2ac23f3d848e8dd40d6814
Ahamed Nismi <mnanismi2000@gmail.com>
2026-02-03 08:31:53 +0530

    Table Created

--- SQL_Quries/SQL_Load/create_table.sql
+++ SQL_Quries/SQL_Load/create_table.sql
@@ -1,8 +1,134 @@
+ /*
+ Use Only if Table Anything Created in Database 
+ 1. Drop tables if they exist (optional, clean start)
 
--- Create Table for Cuurency Exchange 
-CREATE TABLE public.currencyexchange (
-    date date NOT NULL,
-    fromcurrency character varying(10) NOT NULL,
-    tocurrency character varying(10) NOT NULL,
-    exchange double precision
+DROP TABLE IF EXISTS public.sales CASCADE;
+DROP TABLE IF EXISTS public.currencyexchange CASCADE;
+DROP TABLE IF EXISTS public.customer CASCADE;
+DROP TABLE IF EXISTS public.product CASCADE;
+DROP TABLE IF EXISTS public.store CASCADE;
+DROP TABLE IF EXISTS public."date" CASCADE;
+*/
+
+--2. Create tables.
+-- Create Currency Exchange Table 
+CREATE TABLE public.currencyexchange(
+  date date NOT NULL,
+  fromcurrency VARCHAR(10) NOT NULL,
+  tocurrency VARCHAR(10) NOT NULL,
+  exchange DOUBLE PRECISION,
+  PRIMARY KEY (date,fromcurrency,tocurrency)
 );
+
+
+-- Create Customer Table 
+
+CREATE TABLE public.customer (
+    customerkey INT NOT NULL PRIMARY KEY,
+    geoareakey INT,
+    startdt date,
+    enddt date,
+    continent VARCHAR(50),
+    gender VARCHAR(10),
+    title VARCHAR(20),
+    givenname VARCHAR(50),
+    middleinitial VARCHAR(5),
+    surname VARCHAR(50),
+    streetaddress VARCHAR(100),
+    city VARCHAR(50),
+    state VARCHAR(50),
+    statefull VARCHAR(100),
+    zipcode VARCHAR(20),
+    country VARCHAR(50),
+    countryfull VARCHAR(100),
+    birthday date,
+    age INT,
+    occupation VARCHAR(100),
+    company VARCHAR(100),
+    vehicle VARCHAR(100),
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION
+    
+);
+
+-- -- Using double quotes because "date" is a SQL keyword; avoids naming conflicts
+CREATE TABLE public."date" (
+    date date NOT NULL  PRIMARY KEY,
+    datekey INT,
+    year INT,
+    yearquarter VARCHAR(20),
+    yearquarternumber INT,
+    quarter VARCHAR(10),
+    yearmonth VARCHAR(20),
+    yearmonthshort VARCHAR(20),
+    yearmonthnumber INT,
+    month VARCHAR(20),
+    monthshort VARCHAR(10),
+    monthnumber INT,
+    dayofweek VARCHAR(20),
+    dayofweekshort VARCHAR(10),
+    dayofweeknumber INT,
+    workingday INT,
+    workingdaynumber INT
+   
+);
+
+
+-- Create Product Table 
+
+CREATE TABLE public.product(
+    productkey INT NOT NULL PRIMARY KEY,
+    productcode INT,
+    productname VARCHAR(100),
+    manufacturer VARCHAR(100),
+    brand VARCHAR(50),
+    color VARCHAR(30),
+    weightunit VARCHAR(10),
+    weight DOUBLE PRECISION,
+    cost DOUBLE PRECISION,
+    price DOUBLE PRECISION,
+    categorykey INT,
+    categoryname VARCHAR(50),
+    subcategorykey INT,
+    subcategoryname VARCHAR(50)
+);
+
+
+-- Create Store Table 
+CREATE TABLE public.store(
+    storekey INT NOT NULL PRIMARY KEY,
+    storecode INTEGER,
+    geoareakey INTEGER,
+    countrycode VARCHAR(10),
+    countryname VARCHAR(50),
+    state VARCHAR(50),
+    opendate date,
+    closedate date,
+    description VARCHAR(100),
+    squaremeters DOUBLE PRECISION,
+    status VARCHAR(20)
+
+);
+
+
+-- Create Sales  Table 
+
+CREATE TABLE public.sales(
+     orderkey INTEGER NOT NULL,
+    linenumber INTEGER NOT NULL,
+    orderdate date,
+    deliverydate date,
+    customerkey INTEGER,
+    storekey INTEGER,
+    productkey INTEGER,
+    quantity INTEGER,
+    unitprice DOUBLE PRECISION,
+    netprice DOUBLE PRECISION,
+    unitcost DOUBLE PRECISION,
+    currencycode VARCHAR(10),
+    exchangerate DOUBLE PRECISION,
+    PRIMARY KEY (orderkey , linenumber),
+    FOREIGN KEY (customerkey) REFERENCES public.customer(customerkey),
+    FOREIGN KEY (productkey)  REFERENCES public.product (productkey),
+    FOREIGN KEY (storekey)    REFERENCES public.store (storekey)
+ );
--- SQL_Quries/SQL_Load/load_csv.sql
+++ SQL_Quries/SQL_Load/load_csv.sql
@@ -1,7 +1,8 @@
 
 COPY  currencyexchange
-FROM 'D:\Mine\SQL\sql_analysis_colab\csv_files\currencyexchange.csv'
+FROM  'D:\Mine\SQL\sql_analysis_colab\csv_files\currencyexchange.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
 
 
 
