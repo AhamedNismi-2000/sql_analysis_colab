cdfbe0b36d7a5f5ac212e4575d9c38464f8752d4
Ahamed Nismi <mnanismi2000@gmail.com>
2026-02-04 21:13:18 +0530

    Maximum Revnue 2022-2024

--- SQL_Quries/SQL_Operation/aggregation/net_revenue_analysis.sql
+++ SQL_Quries/SQL_Operation/aggregation/net_revenue_analysis.sql
@@ -10,3 +10,16 @@ LEFT JOIN product p ON p.productkey = s.productkey
 GROUP BY 
      p.categoryname ;        
 
+
+-- Find the Maximum of net revenue 2022- 2024
+
+SELECT
+     p.categoryname,
+     ROUND(MAX(CASE WHEN s.orderdate BETWEEN '2022-01-01' AND '2023-12-31' THEN (s.quantity * s.netprice * s.exchangerate)::numeric END),2) AS min_net_revenue_2022,  
+     ROUND(MAX(CASE WHEN s.orderdate BETWEEN '2023-01-01' AND '2024-12-31' THEN (s.quantity * s.netprice * s.exchangerate)::numeric END),2) AS min_net_revenue_2023,  
+     ROUND(MAX(CASE WHEN s.orderdate BETWEEN '2024-01-01' AND '2025-12-31' THEN (s.quantity * s.netprice * s.exchangerate)::numeric END),2) AS min_net_revenue_2024
+FROM sales s
+LEFT JOIN product p ON p.productkey = s.productkey   
+GROUP BY 
+     p.categoryname ;        
+
