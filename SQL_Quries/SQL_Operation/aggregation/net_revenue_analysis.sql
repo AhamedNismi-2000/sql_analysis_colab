003d4cba8dfdf6d7eac568c25b79a67c57ddb832
Ahamed Nismi <mnanismi2000@gmail.com>
2026-02-04 21:14:29 +0530

    Average  net revenue 2022- 2024

--- SQL_Quries/SQL_Operation/aggregation/net_revenue_analysis.sql
+++ SQL_Quries/SQL_Operation/aggregation/net_revenue_analysis.sql
@@ -23,3 +23,17 @@ LEFT JOIN product p ON p.productkey = s.productkey
 GROUP BY 
      p.categoryname ;        
 
+
+-- Find the Average  net revenue 2022- 2024
+
+SELECT
+     p.categoryname,
+     ROUND(AVG(CASE WHEN s.orderdate BETWEEN '2022-01-01' AND '2023-12-31' THEN (s.quantity * s.netprice * s.exchangerate)::numeric END),2) AS min_net_revenue_2022,  
+     ROUND(AVG(CASE WHEN s.orderdate BETWEEN '2023-01-01' AND '2024-12-31' THEN (s.quantity * s.netprice * s.exchangerate)::numeric END),2) AS min_net_revenue_2023,  
+     ROUND(AVG(CASE WHEN s.orderdate BETWEEN '2024-01-01' AND '2025-12-31' THEN (s.quantity * s.netprice * s.exchangerate)::numeric END),2) AS min_net_revenue_2024
+FROM sales s
+LEFT JOIN product p ON p.productkey = s.productkey   
+GROUP BY 
+     p.categoryname ;        
+
+
