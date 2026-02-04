59c5a7de299e72fac5e0f6c3ebce263ca3759e7d
Ahamed Nismi <mnanismi2000@gmail.com>
2026-02-05 00:02:50 +0530

    .vscode files deleted

--- SQL_Quries/SQL_Operation/aggregation/net_revenue_analysis.sql
+++ SQL_Quries/SQL_Operation/aggregation/net_revenue_analysis.sql
@@ -1,4 +1,4 @@
--- Find the Minimum of net revenue 2022- 2024
+-- Find the Minimum  net revenue 2022- 2024
 
 SELECT
      p.categoryname,
@@ -11,7 +11,7 @@ GROUP BY
      p.categoryname ;        
 
 
--- Find the Maximum of net revenue 2022- 2024
+-- Find the Maximum  net revenue 2022- 2024
 
 SELECT
      p.categoryname,
@@ -36,4 +36,22 @@ LEFT JOIN product p ON p.productkey = s.productkey
 GROUP BY 
      p.categoryname ;        
 
+
+-- Find All Revenue 2022 - 2024
+
+SELECT
+     p.categoryname,
+     ROUND(AVG(CASE WHEN s.orderdate BETWEEN '2022-01-01' AND '2023-12-31' THEN (s.quantity * s.netprice * s.exchangerate)::numeric END),2) AS min_net_revenue_2022,  
+     ROUND(AVG(CASE WHEN s.orderdate BETWEEN '2023-01-01' AND '2024-12-31' THEN (s.quantity * s.netprice * s.exchangerate)::numeric END),2) AS min_net_revenue_2023,  
+     ROUND(AVG(CASE WHEN s.orderdate BETWEEN '2024-01-01' AND '2025-12-31' THEN (s.quantity * s.netprice * s.exchangerate)::numeric END),2) AS min_net_revenue_2024,
+     ROUND(MAX(CASE WHEN s.orderdate BETWEEN '2022-01-01' AND '2023-12-31' THEN (s.quantity * s.netprice * s.exchangerate)::numeric END),2) AS min_net_revenue_2022,  
+     ROUND(MAX(CASE WHEN s.orderdate BETWEEN '2023-01-01' AND '2024-12-31' THEN (s.quantity * s.netprice * s.exchangerate)::numeric END),2) AS min_net_revenue_2023,  
+     ROUND(MAX(CASE WHEN s.orderdate BETWEEN '2024-01-01' AND '2025-12-31' THEN (s.quantity * s.netprice * s.exchangerate)::numeric END),2) AS min_net_revenue_2024,
+     ROUND(MIN(CASE WHEN s.orderdate BETWEEN '2022-01-01' AND '2023-12-31' THEN (s.quantity * s.netprice * s.exchangerate)::numeric END),2) AS min_net_revenue_2022,  
+     ROUND(MIN(CASE WHEN s.orderdate BETWEEN '2023-01-01' AND '2024-12-31' THEN (s.quantity * s.netprice * s.exchangerate)::numeric END),2) AS min_net_revenue_2023,  
+     ROUND(MIN(CASE WHEN s.orderdate BETWEEN '2024-01-01' AND '2025-12-31' THEN (s.quantity * s.netprice * s.exchangerate)::numeric END),2) AS min_net_revenue_2024
+FROM sales s
+LEFT JOIN product p ON p.productkey = s.productkey   
+GROUP BY 
+     p.categoryname ;        
 
