2f6459426bde4a63b5e60702b93cc294cd8e4378
Ahamed Nismi <mnanismi2000@gmail.com>
2026-02-06 20:05:22 +0530

    High , low netrevenue fro 2022,2023

--- SQL_Quries/SQL_Operation/aggregation/item_value.sql
+++ SQL_Quries/SQL_Operation/aggregation/item_value.sql
@@ -15,23 +15,32 @@ ORDER BY netprice DESC,quantity DESC;
 
 
 -- Analyse the Netrevenue and Categorize into Several types 
-WITH median_value AS (
-     SELECT 
-          
-          ROUND(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY (quantity*netprice*exchangerate))::numeric ,2 ) AS median 
-     FROM sales
-     WHERE orderdate BETWEEN '2022-01-01'  AND '2023-12-31'
-          
-)
-SELECT * FROM median_value 
+     WITH median_value AS (
+          SELECT 
+               
+               ROUND(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY (quantity*netprice*exchangerate))::numeric,2 ) AS median 
+          FROM sales
+          WHERE orderdate BETWEEN '2022-01-01'  AND '2023-12-31'
+               
+     )
 
-  WHEN median_revenue AS(
-    SELECT orderdate,
-    ROUND(SUM(CASE WHEN (quantity*netprice*exchangerate)::numeric  < mv.median AND 
-    WHERE orderdate BETWEEN '2022-01-01' AND '2022-12-31' 
-    THEN  (quantity*netprice*exchangerate)::numeric )) AS low_new_revenue_2022 ;
-  )
-  FROM sales s
-  LEFT JOIN product p ON s.productkey=p.productkey,
-  median_value
-  GROUP BY orderdate
+     SELECT 
+          p.categoryname, 
+          SUM(CASE WHEN ROUND((quantity*netprice*exchangerate)::numeric ,2) < mv.median AND 
+          orderdate BETWEEN '2022-01-01' AND '2022-12-31' THEN ROUND((quantity*netprice*exchangerate)::numeric ,2) END ) AS low_revenue_2022,
+
+          SUM(CASE WHEN ROUND((quantity*netprice*exchangerate)::numeric ,2 ) >= mv.median 
+          AND orderdate BETWEEN '2022-01-01' AND '2022-12-31' THEN ROUND((quantity*netprice*exchangerate)::numeric ,2) END ) AS high_net_revenue2022,
+          
+          SUM(CASE WHEN ROUND((quantity*netprice*exchangerate)::numeric ,2 ) < mv.median 
+          AND orderdate BETWEEN '2023-01-01' AND '2023-12-31' THEN ROUND((quantity*netprice*exchangerate)::numeric ,2) END ) AS high_net_revenue2023,
+
+          SUM(CASE WHEN ROUND((quantity*netprice*exchangerate)::numeric ,2 ) >= mv.median 
+          AND orderdate BETWEEN '2023-01-01' AND '2023-12-31' THEN ROUND((quantity*netprice*exchangerate)::numeric ,2) END ) AS high_net_revenue2023
+
+          FROM sales s
+          LEFT JOIN product p ON p.productkey = s.productkey,
+          median_value as mv
+          GROUP BY  p.categoryname;
+          
+               
