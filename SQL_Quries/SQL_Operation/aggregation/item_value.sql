441d2e2815fb21c61da065300327fa2d3b8de088
Ahamed Nismi <mnanismi2000@gmail.com>
2026-02-07 13:40:22 +0530

    query by category modified

--- SQL_Quries/SQL_Operation/aggregation/item_value.sql
+++ SQL_Quries/SQL_Operation/aggregation/item_value.sql
@@ -17,7 +17,6 @@ ORDER BY netprice DESC,quantity DESC;
 -- Analyse the Netrevenue and Categorize into Several types 
      WITH median_value AS (
           SELECT 
-               
                ROUND(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY (quantity*netprice*exchangerate))::numeric,2 ) AS median 
           FROM sales
           WHERE orderdate BETWEEN '2022-01-01'  AND '2023-12-31'
@@ -30,19 +29,33 @@ ORDER BY netprice DESC,quantity DESC;
           orderdate BETWEEN '2022-01-01' AND '2022-12-31' THEN ROUND((quantity*netprice*exchangerate)::numeric ,2) END ) AS low_revenue_2022,
 
           SUM(CASE WHEN ROUND((quantity*netprice*exchangerate)::numeric ,2 ) >= mv.median 
-          AND orderdate BETWEEN '2022-01-01' AND '2022-12-31' THEN ROUND((quantity*netprice*exchangerate)::numeric ,2) END ) AS high_net_revenue2022,
+          AND orderdate BETWEEN '2022-01-01' AND '2022-12-31' THEN ROUND((quantity*netprice*exchangerate)::numeric ,2) END ) AS high_revenue2022,
           
           SUM(CASE WHEN ROUND((quantity*netprice*exchangerate)::numeric ,2 ) < mv.median 
-          AND orderdate BETWEEN '2023-01-01' AND '2023-12-31' THEN ROUND((quantity*netprice*exchangerate)::numeric ,2) END ) AS high_net_revenue2023,
+          AND orderdate BETWEEN '2023-01-01' AND '2023-12-31' THEN ROUND((quantity*netprice*exchangerate)::numeric ,2) END ) AS low_revenue2023,
 
           SUM(CASE WHEN ROUND((quantity*netprice*exchangerate)::numeric ,2 ) >= mv.median 
-          AND orderdate BETWEEN '2023-01-01' AND '2023-12-31' THEN ROUND((quantity*netprice*exchangerate)::numeric ,2) END ) AS high_net_revenue2023
+          AND orderdate BETWEEN '2023-01-01' AND '2023-12-31' THEN ROUND((quantity*netprice*exchangerate)::numeric ,2) END ) AS high_revenue2023
 
           FROM sales s
           LEFT JOIN product p ON p.productkey = s.productkey,
-          median_value as mv
+          median_value AS mv
           GROUP BY  p.categoryname;
+
+
+          --    Query For Checking Purpose
+
+
+
+
+
+
+
+
+
+
           
+    
 
 
 -- Categorize the sales items into High, medium, Low 
