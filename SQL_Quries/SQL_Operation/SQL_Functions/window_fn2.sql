02aa44d89bec43c1ba172e1776b2d2723af32870
Ahamed Nismi <mnanismi2000@gmail.com>
2026-02-08 18:52:51 +0530

    First Values , Last Value , NTH Value

--- SQL_Quries/SQL_Operation/SQL_Functions/window_fn2.sql
+++ SQL_Quries/SQL_Operation/SQL_Functions/window_fn2.sql
@@ -36,3 +36,20 @@ SELECT
      DENSE_RANK() OVER(ORDER BY COUNT(*) DESC) AS total_dense_rank
      FROM sales
      GROUP BY customerkey
+
+-- First Values , Last Value , NTH Value 
+
+    WITH monthly_revenue AS ( 
+    SELECT   
+        TO_CHAR(orderdate ,'YYYY-MM') AS month ,
+        ROUND(SUM(quantity*exchangerate*netprice)::numeric ,2) AS total_revenue
+        FROM sales
+        WHERE EXTRACT(YEAR FROM orderdate) = 2023
+        GROUP BY  TO_CHAR(orderdate ,'YYYY-MM')
+        ORDER BY  TO_CHAR(orderdate ,'YYYY-MM')
+    )
+    SELECT *,
+    FIRST_VALUE(total_revenue) OVER(ORDER BY month) AS first_month_revenue,
+    LAST_VALUE(total_revenue)  OVER (ORDER BY month ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS last_month_revenue,
+    NTH_VALUE(total_revenue,3)  OVER (ORDER BY month ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS third_month_revenue
+    FROM monthly_revenue 
