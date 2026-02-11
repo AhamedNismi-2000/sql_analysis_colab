99037dcc7bfc6979a7ab8ad0d0d3cd6dda76a027
Ahamed Nismi <mnanismi2000@gmail.com>
2026-02-11 14:48:02 +0530

    total revenue after cleanup null values

---
+++ DBeaver/Scripts/1_revenue_clean.sql
@@ -0,0 +1,13 @@
+WITH sales_data AS (
+   SELECT 
+        customerkey,
+        ROUND(SUM(quantity*exchangerate*netprice)::numeric,2)  AS netrevenue 
+        FROM sales
+        GROUP BY customerkey 
+)
+SELECT 
+    ROUND(AVG(sd.netrevenue::numeric),2) AS avg_netrevenue_customer,
+    ROUND(AVG(COALESCE(sd.netrevenue::numeric)),2) AS avg_netrevenue_all_customer
+    FROM customer c 
+    LEFT JOIN sales_data sd ON
+    c.customerkey = sd.customerkey
